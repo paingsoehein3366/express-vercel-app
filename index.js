bd6499014a29fbcb3b72ee8b7a4ba8b34f963ccd
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2023-10-05 16:28:55 +0900

    login page start

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import express from "express";
+import express, { json } from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Express on vercel");
-})
+app.use(express.json());
+
+app.use(express.static("public"))
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) return res.send(400);
+    console.log("email :", email, ", password :", password);
+    res.send(200);
+});
+app.get("/upload", (req, res) => {
+    console.log("email :", req.body.email);
+    res.send(200);
+});
 
 app.listen(5000, () => {
     console.log("server running port 5000");
