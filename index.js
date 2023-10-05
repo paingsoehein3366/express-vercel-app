6a77782230dc1a5003c7b78234d0b8873964e53f
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2023-10-05 16:44:37 +0900

    third index.js update

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static("public"))
-
 app.get("/", (req, res) => {
     const Array = [
         { id: 1, name: "paing", email: "paing@email.com", password: "paing" },
         { id: 1, name: "soe", email: "soe@email.com", password: "soe" }
     ];
     res.send(Array);
+});
+app.get("/login", (req, res) => {
+    const { email, password } = req.body;
+    const EmailTag = email;
+    const PasswordTag = password;
+    console.log(EmailTag, PasswordTag);
+    res.send(app.use(express.static("public")));
 });
 app.listen(5000, () => {
     console.log("server running port 5000");
