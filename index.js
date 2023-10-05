29c040eda3c4d81314bb45789c5596517a4e3ca9
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2023-10-05 16:34:17 +0900

    index.js update

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static("public"))
-
 app.post("/login", (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) return res.send(400);
     console.log("email :", email, ", password :", password);
     res.send(200);
 });
-app.get("/upload", (req, res) => {
-    console.log("email :", req.body.email);
-    res.send(200);
+app.get("/", (req, res) => {
+    res.send("hello express");
 });
 
 app.listen(5000, () => {
